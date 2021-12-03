9d45ef6e634519a9a117d9e3ce857afe123b5879
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2021-12-03 16:44:27 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,11 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :items_name, :items_info, :image
-    with_options numericality: { other_than: 0, message: "can't be blank" } do
-      validates :category_id, :items_status_id, :shipping_fee_id, :prefecture_id, :shipping_date_id
-    end
     validates :items_price,
               numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-                              message: 'is invalid. Input half-width numbers' }
+                              message: 'is invalid. Input half-width numbers (between 300 ~ 9,999,999)' }
+  end
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
+    validates :category_id, :items_status_id, :shipping_fee_id, :prefecture_id, :shipping_date_id
   end
 end
