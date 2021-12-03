69a9fc1d3d0b0eecb3bc656d36f82f5cf9c7f61d
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2021-12-03 18:05:15 +0900

    itemモデル単体テスト修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Item, type: :model do
   end
 
   context '商品情報の入力内容に問題がない場合' do
-    it 'すべての項目が正しく入力されていれば商品登録できる' do
+    it 'すべての項目が正しく入力されていれば商品登録ができる' do
       expect(@item).to be_valid
     end
   end
@@ -22,28 +22,28 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Items info can't be blank")
     end
-    it 'category_idがカラでは商品登録ができない' do
-      @item.category_id = ''
+    it 'category_idが0では商品登録ができない' do
+      @item.category_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank")
     end
-    it 'items_status_idがカラでは商品登録できない' do
-      @item.items_status_id = ''
+    it 'items_status_idが0では商品登録ができない' do
+      @item.items_status_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include("Items status can't be blank")
     end
-    it 'shipping_fee_idがカラでは商品登録ができない' do
-      @item.shipping_fee_id = ''
+    it 'shipping_fee_idが0では商品登録ができない' do
+      @item.shipping_fee_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
     end
-    it 'prefectureがカラでは商品登録ができない' do
-      @item.prefecture_id = ''
+    it 'prefecture_idが0では商品登録ができない' do
+      @item.prefecture_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefecture can't be blank")
     end
-    it 'shipping_date_idがカラでは商品登録ができない' do
-      @item.shipping_date_id = ''
+    it 'shipping_date_idが0では商品登録ができない' do
+      @item.shipping_date_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping date can't be blank")
     end
@@ -52,11 +52,18 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Items price can't be blank")
     end
-    it 'item_priceが規定の金額範囲でなければ商品登録ができない' do
+    it 'item_priceが300未満では商品登録ができない' do
       @item.items_price = '299'
       @item.valid?
       expect(@item.errors.full_messages).to include('Items price is invalid. Input half-width numbers (between 300 ~ 9,999,999)')
-      @item.items_price = '10_000_000'
+    end
+    it 'item_priceが9999999を超過していたら商品登録ができない' do
+      @item.items_price = '10000000'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Items price is invalid. Input half-width numbers (between 300 ~ 9,999,999)')
+    end
+    it 'item_priceに半角数字以外が含まれている場合は商品登録ができない' do
+      @item.items_price = '123４5'
       @item.valid?
       expect(@item.errors.full_messages).to include('Items price is invalid. Input half-width numbers (between 300 ~ 9,999,999)')
     end
