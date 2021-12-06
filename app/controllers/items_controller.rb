2af731c58cd254fc06ed7396438f22339c1c2147
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2021-12-06 18:40:40 +0900

    set_itemメソッド(private)の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,18 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit 
-    @item = Item.find(params[:id])
     unless current_user[:id] == @item.user[:id]
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(params[:id])
     else
@@ -52,5 +50,9 @@ class ItemsController < ApplicationController
       :items_price,
       :image
     ).merge(user_id: current_user[:id])
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
