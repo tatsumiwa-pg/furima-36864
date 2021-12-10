cdbbd58e84baab0eacf2528055fb2f87bd28dc7f
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2021-12-10 20:10:09 +0900

    shipping_addressインスタンスの作成（暫定）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,8 +9,9 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     if @order.valid?
-      pay_item
       @order.save
+      shipping_address = ShippingAddress.create(shipping_address_params)
+      pay_item
       redirect_to root_path
     else
       render :index
@@ -21,6 +22,10 @@ class OrdersController < ApplicationController
 
   def order_params
     params.permit(:item_id, :token).merge(user_id: current_user[:id])
+  end
+
+  def shipping_address_params
+    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(order_id: @order[:id])
   end
 
   def set_item
