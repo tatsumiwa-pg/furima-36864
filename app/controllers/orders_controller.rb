721c460939fadae9b24619fb8a1bbe3c07bb0b9e
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2021-12-10 19:40:57 +0900

    createアクションの作成（order部分のみ）
    - orderモデルのバリデーション設定
    - set_itemメソッド（Private）の作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,38 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
 
   def index
     @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:item_id, :token).merge(user_id: current_user[:id])
+  end
+
+  def set_item
     @item = Item.find(params[:item_id])
   end
-  
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item[:items_price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,7 @@ class Order < ApplicationRecord
   belongs_to :item
   belongs_to :user
   has_one    :shipping_addresses
+
+  attr_accessor :token
+  validates :token, presence: true
 end
