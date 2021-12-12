640a0c210bd6a6454b843a86179609dfb720a1ee
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2021-12-13 00:08:55 +0900

    バリデーションの修正 / モデル単体テスト

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -2,7 +2,11 @@ class OrderShippingAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token, :item_id, :user_id
 
-  validates :postal_code, :city, :house_number, :building_name, :phone_number, :token, presence: true
+  with_options presence: true do 
+    validates :token, :city, :house_number, :user_id, :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-) & half-width numbers(e.g. 000-0000)"}
+    validates :phone_number, numericality: { message: "is invalid. Input only half-width numbers" }
+  end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
---
+++ spec/models/order_shipping_address_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+
+RSpec.describe OrderShippingAddress, type: :model do
+  describe '購入情報の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_shipping_address = FactoryBot.build(:order_shipping_address, user_id: user[:id], item_id: item[:id])
+      sleep 0.02
+    end
+
+    context '購入情報の入力内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できる' do
+        expect(@order_shipping_address).to be_valid
+      end
+      it 'building_nameはカラでも保存できる' do
+        @order_shipping_address.building_name = ''
+        expect(@order_shipping_address).to be_valid
+      end
+    end
+
+    context '購入情報の入力内容に問題がある場合' do
+      it 'tokenがカラだと保存できない' do
+        @order_shipping_address.token = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Token can't be blank")
+      end
+      it 'postal_codeが空だと保存できない' do
+        @order_shipping_address.postal_code = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できない' do
+        @order_shipping_address.postal_code = '1234567'
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include(
+            "Postal code is invalid. Include hyphen(-) & half-width numbers(e.g. 000-0000)"
+          )
+      end
+      it 'prefectureを選択していないと保存できない' do
+        @order_shipping_address.prefecture_id = 0
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'cityがカラだと保存できない' do
+        @order_shipping_address.city = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'house_numberがカラだと保存できない' do
+        @order_shipping_address.house_number = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("House number can't be blank")
+      end
+      it 'phone_numberがカラだと保存できない' do
+        @order_shipping_address.phone_number = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberが数字のみで構成されていなければ保存できない' do
+        @order_shipping_address.phone_number = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Phone number is invalid. Input only half-width numbers")
+      end
+      it 'userが紐付いていないと保存できない' do
+        @order_shipping_address.user_id = nil
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("User can't be blank")
+      end
+      it 'itemが紐付いていないと保存できない' do
+        @order_shipping_address.item_id = nil
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Item can't be blank")
+      end
+    end
+  end
+end
