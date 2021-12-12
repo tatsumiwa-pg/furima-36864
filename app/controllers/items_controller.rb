78439283890063d17167c9470957370b94e998b6
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2021-12-12 19:15:35 +0900

    商品一覧画面でのSoldOut表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,3 @@
-require 'benchmark'
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
